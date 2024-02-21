1a61958baff8d50228d603ba8ea60ab7072ab2c0
Roma <nikitinroma2605@gmail.com>
2024-02-21 19:32:01 +0200

    [feat] Created mapper

---
+++ TestingApi/Helpers/MappingProfiles.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using TestingApi.Dto;
+using TestingApi.Models;
+
+namespace TestingApi.Helpers;
+
+public class MappingProfiles : Profile {
+    public MappingProfiles() {
+        CreateMap<Test, TestDto>()
+            .ForMember(dest => dest.Difficulty, 
+                opt => opt
+                    .MapFrom(src => src.Difficulty.ToString()));
+            
+        CreateMap<Test, TestDto>()
+            .ForMember(dest => dest.Difficulty, 
+                opt => opt
+                    .MapFrom(src => src.Difficulty.ToString()));
+
+        CreateMap<TestDto, Test>()
+            .ForMember(dest => dest.Difficulty,
+                opt => opt
+                    .MapFrom(src => Enum.Parse(typeof(TestDifficulty), src.Difficulty)));
+
+        
+    }
+}
