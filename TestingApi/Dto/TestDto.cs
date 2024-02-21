ea2b5586e5f2a0bdd4dcfff6742ee36756a5d396
Roma <nikitinroma2605@gmail.com>
2024-02-21 19:31:40 +0200

    [feat] Created test dtos

---
+++ TestingApi/Dto/TestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TestingApi.Dto;
+
+public class TestDto
+{
+    public string Name { get; set; } = null!;
+    public string Subject { get; set; } = null!;
+    public int Duration { get; set; }
+    public bool IsPublished { get; set; }
+    public string Difficulty { get; set; } = null!;
+}
