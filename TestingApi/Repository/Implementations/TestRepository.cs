380b17d5e75c3b61e9cea8b47971ff315ab0bd40
Roma <nikitinroma2605@gmail.com>
2024-02-21 18:05:30 +0200

    [feat] Test repo implemented

---
+++ TestingApi/Repository/Implementations/TestRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestingApi.Data;
+using TestingApi.Models;
+using TestingApi.Repository.Abstractions;
+
+namespace TestingApi.Repository.Implementations;
+
+public class TestRepository : GenericRepository<Test>, ITestRepository
+{
+    public TestRepository(DataContext dataContext) : base(dataContext)
+    {
+    }
+
+    public async Task<ICollection<Test>> GetAllTests()
+    {
+        return await _dbSet.ToListAsync();
+    }
+}
