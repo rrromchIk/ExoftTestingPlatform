17a052e3d417bdb80ccfa25ba0fd7b01948027c5
Roma <nikitinroma2605@gmail.com>
2024-02-21 19:33:28 +0200

    [feat] Created controller

---
+++ TestingApi/Controllers/TestsController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TestingApi.Dto;
+using TestingApi.Dto.Response;
+using TestingApi.Services.Abstractions;
+
+namespace TestingApi.Controllers;
+
+[ApiController]
+[Route("api/tests")]
+public class TestsController : ControllerBase
+{
+    private readonly ITestService _testService;
+    private readonly ILogger<TestsController> _logger;
+
+    public TestsController(ITestService testService, IMapper mapper, ILogger<TestsController> logger) {
+        _testService = testService;
+        _logger = logger;
+    }
+    
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<TestResponseDto>))]
+    public async Task<IActionResult> GetAllTests()
+    {
+        var response = await _testService.GetAllTestsAsync();
+        return Ok(response);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(TestDto))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetTestById([FromRoute] Guid id) {
+        if (!await _testService.ExistsAsync(id)) 
+            return NotFound();
+
+        var response = await _testService.GetByIdAsync(id);
+        return Ok(response);
+    }
+}
