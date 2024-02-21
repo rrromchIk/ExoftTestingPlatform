f8d58404fb9a3708585ebfe2a6dd9e8408af6ba4
Roma <nikitinroma2605@gmail.com>
2024-02-21 13:34:09 +0200

    [feat] Created Interfaces for test and question repo

---
+++ TestingApi/Repository/Abstractions/IQuestionRepository.cs
@@ -0,0 +1,9 @@
+﻿using TestingApi.Models;
+using TestingApi.Repository.Abstractions;
+
+namespace TestingApi.Repo.Abstractions;
+
+public interface IQuestionRepository : IGenericRepository<Question>
+{
+    Task<ICollection<Question>> GetQuestionsByTestId(Guid testId);
+}
---
+++ TestingApi/Repository/Abstractions/ITestRepository.cs
@@ -0,0 +1,9 @@
+﻿using TestingApi.Models;
+using TestingApi.Repo.Abstractions;
+
+namespace TestingApi.Repository.Abstractions;
+
+public interface ITestRepository : IGenericRepository<Test>
+{
+    Task<ICollection<Test>> GetAllTests();
+}
