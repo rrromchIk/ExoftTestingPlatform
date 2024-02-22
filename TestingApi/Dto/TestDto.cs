cc84517e87d10661916ec1756ba2a71b5b985a06
Roma <nikitinroma2605@gmail.com>
2024-02-22 21:59:34 +0200

    [refactor] added validation to testDto

--- TestingApi/Dto/TestDto.cs
+++ TestingApi/Dto/TestDto.cs
@@ -1,10 +1,28 @@
-﻿namespace TestingApi.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestingApi.Dto;
 
 public class TestDto
 {
+    [Required(ErrorMessage = "Name is required")]
+    [MaxLength(25, ErrorMessage = "Name cannot exceed 25 characters")]
+    [MinLength(2, ErrorMessage = "Name must be at least 2 characters")]
     public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Subject is required")]
+    [MinLength(2, ErrorMessage = "Subject must be at least 2 characters")]
     public string Subject { get; set; } = null!;
+
+    [Required(ErrorMessage = "Duration is required")]
+    [Range(1, int.MaxValue, ErrorMessage = "Duration must be greater than 0")]
     public int Duration { get; set; }
-    public bool IsPublished { get; set; }
+
+    [Required(ErrorMessage = "Difficulty is required")]
+    [RegularExpression(
+        @"^(?i)(easy|medium|hard)$",
+        ErrorMessage = "Difficulty must be: easy, medium or hard (case-insensitive)"
+    )]
     public string Difficulty { get; set; } = null!;
 }
+
+
