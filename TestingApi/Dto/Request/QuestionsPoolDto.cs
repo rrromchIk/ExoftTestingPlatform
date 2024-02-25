f8ab4f8851dc8d8a957d51171266168c419284ab
Roma <nikitinroma2605@gmail.com>
2024-02-26 00:27:43 +0200

    [feat] added validation to QuestionsPoolDto.cs

--- TestingApi/Dto/Request/QuestionsPoolDto.cs
+++ TestingApi/Dto/Request/QuestionsPoolDto.cs
@@ -1,11 +1,25 @@
-﻿using TestingApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TestingApi.Dto.Request;
 
 public class QuestionsPoolDto
 {
+    [Required(ErrorMessage = "TestId is required")]
     public Guid TestId { get; set; }
+    
+    [Required(ErrorMessage = "Name is required")]
+    [MaxLength(25, ErrorMessage = "Name cannot exceed 25 characters")]
+    [MinLength(2, ErrorMessage = "Name must be at least 2 characters")]
     public string Name { get; set; } = null!;
+    
+    [Required(ErrorMessage = "NumOfQuestionsToBeGenerated is required")]
+    [Range(1, int.MaxValue, ErrorMessage = "NumOfQuestionsToBeGenerated must be greater than 0")]
     public int NumOfQuestionsToBeGenerated { get; set; }
+    
+    [Required(ErrorMessage = "GenerationStrategy is required")]
+    [RegularExpression(
+        @"^(?i)(sequentially|randomly)$",
+        ErrorMessage = "GenerationStrategy must be: sequentially or randomly (case-insensitive)"
+    )]
     public string GenerationStrategy { get; set; } = null!;
 }
