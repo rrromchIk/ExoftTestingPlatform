5e82ed3840bc291132fe3a455c8bacb2832ce954
Roma <nikitinroma2605@gmail.com>
2024-03-10 13:09:43 +0200

    [config] Auth setting changed

--- Security/Settings/AuthSettings.cs
+++ Security/Settings/AuthSettings.cs
@@ -1,9 +1,15 @@
-﻿namespace Security.Settings;
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Security.Settings;
 
 public class AuthSettings
 {
     public string SecretKey { get; set; }
     public string Issuer { get; set; }
     public string Audience { get; set; }
-    public int Lifetime { get; set; }
+    public int AccessTokenExpirationMinutes { get; set; }
+    public int RefreshTokenExpirationMinutes { get; set; }
+    public SecurityKey SymmetricSecurityKey { get =>
+        new SymmetricSecurityKey(Encoding.ASCII.GetBytes("AuthSettings:SecretKey")); }
 }
