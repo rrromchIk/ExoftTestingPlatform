cfa4cc01ea9836c0d9989877508b1a8c6b015f42
Roma <nikitinroma2605@gmail.com>
2024-03-16 13:09:08 +0200

    [feat] deleting user through api to achieve sync between security and api users

--- TestingApi/Services/Implementations/UserService.cs
+++ TestingApi/Services/Implementations/UserService.cs
@@ -1,8 +1,10 @@
 ﻿using System.Linq.Expressions;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TestingApi.Constants;
 using TestingApi.Data;
@@ -24,13 +26,15 @@ public class UserService : IUserService
     private readonly ILogger<UserService> _logger;
     private readonly HttpClient _httpClient;
     private readonly SecurityHttpClientConstants _securityHttpClientConstants;
+    private readonly ICurrentUserService _currentUserService;
 
     public UserService(DataContext dataContext, ILogger<UserService> logger,
         IMapper mapper, IHttpClientFactory httpClientFactory,
-        IOptions<SecurityHttpClientConstants> securityHttpClientConstants)
+        IOptions<SecurityHttpClientConstants> securityHttpClientConstants, ICurrentUserService currentUserService)
     {
         _dataContext = dataContext;
         _mapper = mapper;
+        _currentUserService = currentUserService;
         _securityHttpClientConstants = securityHttpClientConstants.Value;
         _httpClient = httpClientFactory.CreateClient(_securityHttpClientConstants.ClientName);
         _logger = logger;
@@ -98,7 +102,6 @@ public class UserService : IUserService
         var jsonContent = JsonSerializer.Serialize(userSignUpDto);
         var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
-        _logger.LogDebug(_securityHttpClientConstants.RegisterEndpoint);
         var httpResponse = await _httpClient.PostAsync(
             _httpClient.BaseAddress + _securityHttpClientConstants.RegisterEndpoint,
             content,
@@ -106,34 +109,18 @@ public class UserService : IUserService
         );
 
         if (httpResponse.StatusCode != HttpStatusCode.OK)
-            throw new ApiException("Error while creating user", (int)httpResponse.StatusCode);
-
+        {
+            var problemDetails = await HandleHttpResponse<ProblemDetails>(httpResponse, cancellationToken);
+            throw new ApiException(problemDetails.Detail, (int)httpResponse.StatusCode);
+        }
+        
         var responseBody = await httpResponse.Content.ReadAsStringAsync(cancellationToken);
         _logger.LogInformation("Response body: {r}", responseBody);
 
-        await using var stream = await httpResponse.Content.ReadAsStreamAsync(cancellationToken);
-
-
-        var securityUserResponseDto = await JsonSerializer.DeserializeAsync<SecurityUserResponseDto>(
-            stream,
-            new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            },
-            cancellationToken
-        );
+        var securityUserResponseDto = await HandleHttpResponse<SecurityUserResponseDto>(httpResponse, cancellationToken);
         _logger.LogInformation("Security response: {r}", JsonSerializer.Serialize(securityUserResponseDto));
 
         var userToAdd = _mapper.Map<User>(securityUserResponseDto);
-
-        // var collision = await _dataContext.Users
-        //     .AnyAsync(
-        //         u => u.Email == userToAdd.Email,
-        //         cancellationToken
-        //     );
-        //
-        // if (collision)
-        //     throw new ApiException("User email has to be unique", StatusCodes.Status409Conflict);
         var createdUser = await _dataContext.AddAsync(userToAdd, cancellationToken);
 
         await _dataContext.SaveChangesAsync(cancellationToken);
@@ -165,10 +152,42 @@ public class UserService : IUserService
 
     public async Task DeleteUserAsync(Guid id, CancellationToken cancellationToken = default)
     {
+        _logger.LogInformation("Access token: {a}", _currentUserService.AccessTokenRaw);
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
+            _currentUserService.AccessTokenRaw);
+        var httpResponse = await _httpClient.DeleteAsync(
+            _httpClient.BaseAddress + _securityHttpClientConstants.UpdateEndpoint + $"/{id}",
+            cancellationToken
+        );
+
+        _logger.LogInformation("Response: Status {s}, Body: {b}",
+            httpResponse.StatusCode, await httpResponse.Content.ReadAsStringAsync(cancellationToken));
+        
+        if (httpResponse.StatusCode != HttpStatusCode.NoContent)
+        {
+            throw new ApiException("Error while deleting user", (int)httpResponse.StatusCode);
+        }
+        
         var userToDelete = await _dataContext.Users
             .FirstAsync(e => e.Id == id, cancellationToken);
 
         _dataContext.Remove(userToDelete);
         await _dataContext.SaveChangesAsync(cancellationToken);
+    }
+
+
+    private static async Task<T> HandleHttpResponse<T>(HttpResponseMessage httpResponse, CancellationToken cancellationToken)
+    {
+        await using var stream = await httpResponse.Content.ReadAsStreamAsync(cancellationToken);
+        var securityUserResponseDto = await JsonSerializer.DeserializeAsync<T>(
+            stream,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            },
+            cancellationToken
+        );
+
+        return securityUserResponseDto;
     }
 }
