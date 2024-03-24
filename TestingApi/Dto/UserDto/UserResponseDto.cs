37dea90f63d2af379a5e9cdc2b84f74db8e5eeba
Roma <nikitinroma2605@gmail.com>
2024-03-25 00:08:57 +0200

    [fix] added EmailConfirmed prop to UserResponseDto

--- TestingApi/Dto/UserDto/UserResponseDto.cs
+++ TestingApi/Dto/UserDto/UserResponseDto.cs
@@ -5,6 +5,7 @@ public class UserResponseDto : BaseResponseDto
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
+    public bool EmailConfirmed { get; set; }
     public string ProfilePictureFilePath { get; set; } = null!;
     public string UserRole { get; set; } = null!;
 }
