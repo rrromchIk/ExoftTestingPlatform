c79642626ac8a552e57c77b4f52557d25d086f09
Roma <nikitinroma2605@gmail.com>
2024-04-11 18:08:17 +0300

    [fix] fixed check for template existence when creating test from template

--- TestingApi/Controllers/TestsController.cs
+++ TestingApi/Controllers/TestsController.cs
@@ -66,7 +66,7 @@ public class TestsController : ControllerBase
         CancellationToken cancellationToken)
     {
         var templateId = testWithQuestionsPoolsDto.TemplateId;
-        if (templateId != null && await _testTmplService.TestTmplExistsAsync(
+        if (templateId != null && !await _testTmplService.TestTmplExistsAsync(
                 templateId.GetValueOrDefault(),
                 cancellationToken
             ))
